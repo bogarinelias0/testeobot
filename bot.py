db13ec13a648c4ab2ec8911bac2bef45c2897ab7
bogarinelias0 <bogarinelias9@gmail.com>
2021-08-12 14:58:13 -0300

    Cambios

--- bot.py
+++ bot.py
@@ -1,20 +1,67 @@
-import telegram
-from telegram.ext import Updater,CommandHandler
-from telegram import KeyboardButton,ReplyKeyboardMarkup
 import logging
-def start(update, context):
-    update.message.reply_text('hola bienvenido!')
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+import json
+
+listapregs= open("quest.json")
+datos= json.load(listapregs)
+for i in datos["Secciones"]:
+    print(i)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
-    updater = Updater(token="1937480308:AAFxyIBYa9awX4anvEaET-ipS87JDUxU16o",use_context=True)
+def start(update: Update, context: CallbackContext) -> None:
+    """Sends a message with three inline buttons attached."""
+    keyboard = [
+        [
+            InlineKeyboardButton("opcioon", callback_data='1'),
+            InlineKeyboardButton("Opcion 2", callback_data='2'),
+        ],
+        [InlineKeyboardButton("Opcion 3", callback_data='3')],
+    ]
 
-    dp = updater.dispatcher
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
-    dp.add_handler(CommandHandler('start',start))
-    
+    update.message.reply_text('Bienvenido al bot de la UTN, sobre que tematica estas buscando info?', reply_markup=reply_markup)
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    query.edit_message_text(text=f"Selected option: {query.data}")
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Displays info on how to use the bot."""
+    update.message.reply_text("Use /start to test this bot.")
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("1937480308:AAFxyIBYa9awX4anvEaET-ipS87JDUxU16o")
+
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
     updater.idle()
 
-telegram.ReplyKeyboardMarkup( keyboard=True, resize_keyboard=False, one_time_keyboard=False, selective=False, input_field_placeholder=None)
-    
+
+if __name__ == '__main__':
+    main()
